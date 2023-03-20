ceaa3d61c212160ccce35cfe8be647b6edc897bf
Alejandro Villegas <alejandrovillegas799@gmail.com>
2023-03-19 20:12:50 -0600

    Primer intento de log in con selenium

--- BotTradingView.py
+++ BotTradingView.py
@@ -1,5 +1,27 @@
-#Bot para monitorear trading view
+#SERVICIO LOG IN
 
-print("prueba 1 monitor 1")
-print("prueba 1 monitor 2")
-print("Modificaciones Rama Alejandro")
+#LOG IN TRADING VIEW
+
+
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+user= "xxxxx"
+pas="xxxx"
+
+driver= webdriver.Chrome()
+driver.get('https://es.tradingview.com/')
+time.sleep(10)
+
+
+driver.find_element(By.XPATH,'//*[@id="one-tap-signin-container"]').click()
+
+#obj= driver.find_element(By.XPATH,'//*[@id="email-signin__user-name-input__a5b4d798-c74f-439d-95d1-a9167f5e7e02"]')
+#obj.send_keys(user)
+#obj= driver.find_element_by_xpath('//*[@id="email-signin__password-input__a5b4d798-c74f-439d-95d1-a9167f5e7e02"]')
+#obj.send_keys(pas)
+
+#driver.find_element_by_xpath('//*[@id="email-signin__submit-button__a5b4d798-c74f-439d-95d1-a9167f5e7e02"]/span[2]').click()
+
